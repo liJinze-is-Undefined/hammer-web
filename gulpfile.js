7919278c506a0284461109175dd35d88ef6926d3
liJinze-is-Undefined <847090981@qq.com>
2019-03-12 14:18:58 +0800

    hammerWeb

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      rename = require('gulp-rename'),
+      cssnano = require('gulp-cssnano');
+
+
+gulp.task('sass',function(){
+    gulp.src('./src/sass/*.scss')
+    .pipe(sass())
+    .pipe(rename({'suffix' : '.min'}))
+    .pipe(gulp.dest('./dist'));
+})
+
+gulp.task('default',()=>{
+    gulp.watch(['./src/sass*.scss'],['sass']);
+})
